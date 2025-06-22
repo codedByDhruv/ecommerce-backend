0ca067b3d21a22a9b1d073269bca045615c84c5d
codedByDhruv <dhruvstackdev@gmail.com>
2025-06-22 12:37:04 +0530

    feat: delete product images from /uploads when product is removed

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,6 @@
 const Product = require("../models/Product");
+const fs = require("fs");
+const path = require("path");
 
 exports.create = async (req, res) => {
   try {
@@ -68,6 +70,20 @@ exports.update = async (req, res) => {
 };
 
 exports.remove = async (req, res) => {
-  await Product.findByIdAndDelete(req.params.id);
-  res.json({ message: "Product deleted" });
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    product.images.forEach(imagePath => {
+      const fullPath = path.join(__dirname, "..", imagePath);
+      fs.unlink(fullPath, err => {
+        if (err) console.error("Failed to delete image:", fullPath, err.message);
+      });
+    });
+
+    await product.deleteOne(); 
+    res.json({ message: "Product and images deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
